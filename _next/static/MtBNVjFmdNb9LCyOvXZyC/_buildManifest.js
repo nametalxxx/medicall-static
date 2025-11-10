91587893e3ae6e82abc5b4f943313bb9f06aec8d
nametalxxx <nametalxxx@gmail.com>
2025-11-10 19:48:37 +0330

    Updates

--- _next/static/MtBNVjFmdNb9LCyOvXZyC/_buildManifest.js
+++ _next/static/MtBNVjFmdNb9LCyOvXZyC/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/medicall-static//_next/:path+",
+        "destination": "/medicall-static/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
